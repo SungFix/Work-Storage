622e7ca8c487dd6c111a0f4229954b6b000c3d96
Sung_Fix <lu1s.0t47io@gmail.com>
2026-09-02 20:11:45 -0300

    chore: connect Work Storage to Firebase project

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
-// Preencha este objeto com a configuração Web do projeto Firebase.
-// Estes identificadores são públicos por design; a segurança real fica nas regras do Firestore.
+// Configuração Web pública do projeto Firebase Work-Storage.
+// A segurança dos dados depende do Firebase Authentication + regras do Firestore.
 window.WORK_STORAGE_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyDIIfqJIAUfQrJmu4BX_e-idEMfRuyjE7k",
+  authDomain: "work-storage-36c1c.firebaseapp.com",
+  projectId: "work-storage-36c1c",
+  storageBucket: "work-storage-36c1c.firebasestorage.app",
+  messagingSenderId: "388484143303",
+  appId: "1:388484143303:web:ee61982add5930643846e2",
+  measurementId: "G-8ZRF3ZBTPG"
 };
